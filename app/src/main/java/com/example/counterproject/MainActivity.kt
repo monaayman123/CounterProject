f8fabd51eb2209667d08e1ce4e6c4f1abd9b3172
Robert Romany <ropertromany001@gmail.com>
2025-09-27 21:02:48 +0300

    Ro2

--- app/src/main/java/com/example/counterproject/MainActivity.kt
+++ app/src/main/java/com/example/counterproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -18,30 +19,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CounterProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Robert2",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Robert()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterProjectTheme {
-        Greeting("Android")
+fun Robert(modifier: Modifier = Modifier) {
+    Column (modifier = Modifier.fillMaxSize()){
+        Text("Robert")
     }
+
+
 }
