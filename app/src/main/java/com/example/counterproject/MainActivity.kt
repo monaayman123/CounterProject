5bcb0cf52cdbe2d60ea0ff9905aeb1946aa87f72
Robert Romany <ropertromany001@gmail.com>
2025-09-27 21:17:02 +0300

    Ro2

--- app/src/main/java/com/example/counterproject/MainActivity.kt
+++ app/src/main/java/com/example/counterproject/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.counterproject.ui.theme.CounterProjectTheme
@@ -27,7 +29,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Robert(modifier: Modifier = Modifier) {
-    Column (modifier = Modifier.fillMaxSize()){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
         Text("Robert")
     }
 
